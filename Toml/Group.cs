1171a10911f30ef25b496e08fbf0af5df6aaf006
shaoyuan <84977043@qq.com>
2017-01-15 13:24:40 +0800

    Update Group.cs (#4)
    * Update Group.cs
    
    解决TryGetGroup不存在的key时抛出异常（如果抛出了异常有违Try的意义）的问题。
    
    * Update Group.cs
    
    delete commented code block

--- Toml/Group.cs
+++ Toml/Group.cs
@@ -197,15 +197,13 @@ namespace Toml
                 group = this;
                 return true;
             }
-
-            Group child = _children[keyParts.First()] as Group;
-            if (child == null)
+            Group child;
+            if(_children.TryGetValue(keyParts.First(), out child))
             {
-                group = null;
-                return false;
+                return child.TryGetGroup(keyParts.Skip(1), out group);
             }
-
-            return child.TryGetGroup(keyParts.Skip(1), out group);
+            group = null;
+            return false;
         }
 
         /// <summary>
